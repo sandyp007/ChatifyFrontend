1a690e5d2e78661e8ccc549b3057dc22146e436e
sandyp007 <sandyp8418@gmail.com>
2023-04-18 15:24:31 +0530

    s

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; 
+import './main.scss';
+import {Provider} from 'react-redux';
+import store from './store/index.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { positions, transitions, Provider as AlertProvider} from 'react-alert';
+import alertTemplate from 'react-alert-template-basic';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const options = {
+  timeout : 5000,
+  positions: positions.BOTTOM_CENTER,
+  transitions : transitions.SCALE
+}
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+
+  root.render(
+  <Provider store={store}>
+     <AlertProvider template={alertTemplate} {...options} >   
+      <App />   
+     </AlertProvider>
+    </Provider>
+    ,
+ ); 
+
+ 
